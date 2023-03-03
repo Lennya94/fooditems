308a4038fddeb952b64b48c2c2f6396b9a3dd52e
Lennya94 <53213636+Lennya94@users.noreply.github.com>
2023-03-03 04:04:53 +0000

    Add files via upload

---
+++ qbfooditems.lua
@@ -0,0 +1,221 @@
+['cheetos'] 	= {
+	['name'] = 'cheetos', 			
+	['label'] = 'cheetos', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'cheetos.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+	
+['dcaramel'] 	= {
+	['name'] = 'dcaramel', 			
+	['label'] = 'dcaramel', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'dcaramel.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['diam_bar'] 	= {
+	['name'] = 'diam_bar', 			
+	['label'] = 'diam_bar', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'diam_bar.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['doritos'] 	= {
+	['name'] = 'doritos', 			
+	['label'] = 'doritos', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'doritos.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['mccoys'] 	= {
+	['name'] = 'mccoys', 			
+	['label'] = 'mccoys', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'mccoys.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['milkybar'] 	= {
+	['name'] = 'milkybar', 			
+	['label'] = 'milkybar', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'milkybar.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['milkybuttons'] 	= {
+	['name'] = 'milkybuttons', 			
+	['label'] = 'milkybuttons', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'milkybuttons.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['milkyway'] 	= {
+	['name'] = 'milkyway', 			
+	['label'] = 'milkyway', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'milkyway.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['minieggs'] 	= {
+	['name'] = 'minieggs', 			
+	['label'] = 'minieggs', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'minieggs.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['quarter_pounder'] 	= {
+	['name'] = 'quarter_pounder', 			
+	['label'] = 'quarter_pounder', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'quarter_pounder.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['quavers'] 	= {
+	['name'] = 'quavers', 			
+	['label'] = 'quavers', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'quavers.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['ripple'] 	= {
+	['name'] = 'ripple', 			
+	['label'] = 'ripple', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'ripple.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['sensations'] 	= {
+	['name'] = 'sensations', 			
+	['label'] = 'sensations', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'sensations.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['squares'] 	= {
+	['name'] = 'squares', 			
+	['label'] = 'squares', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'squares.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['twirl'] 	= {
+	['name'] = 'twirl', 			
+	['label'] = 'twirl', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'twirl.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['twix'] 	= {
+	['name'] = 'twix', 			
+	['label'] = 'twix', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'twix.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['yorkie'] 	= {
+	['name'] = 'yorkie', 			
+	['label'] = 'yorkie', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'yorkie.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
