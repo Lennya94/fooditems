df11bc23ece586ee9219c83ff9bfb483828503c6
Lennya94 <53213636+Lennya94@users.noreply.github.com>
2023-03-03 03:59:30 +0000

    Add files via upload

---
+++ oxitems.lua
@@ -0,0 +1,203 @@
+['cheetos'] = {
+		label = 'cheetos',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious cheetos'
+		},
+	},
+
+	['dcaramel'] = {
+		label = 'dcaramel',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious dcaramel'
+		},
+	},
+
+	['diam_bar'] = {
+		label = 'diambar',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious diambar'
+		},
+	},
+
+	['doritos'] = {
+		label = 'doritos',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious doritos'
+		},
+	},
+
+	['mccoys'] = {
+		label = 'mccoys',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious mccoys'
+		},
+	},
+
+	['milkybar'] = {
+		label = 'milkybar',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious milkybar'
+		},
+	},
+
+	['milkybuttons'] = {
+		label = 'milkybuttons',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious milkybuttons'
+		},
+	},
+
+	['milkyway'] = {
+		label = 'milkyway',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious milkyway'
+		},
+	},
+
+	['minieggs'] = {
+		label = 'minieggs',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious minieggs'
+		},
+	},
+
+	['quarter_pounder'] = {
+		label = 'quarterpounder',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious quarterpounder'
+		},
+	},
+
+	['quavers'] = {
+		label = 'quavers',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious quavers'
+		},
+	},
+
+	['ripple'] = {
+		label = 'ripple',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious ripple'
+		},
+	},
+
+	['sensations'] = {
+		label = 'sensations',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious sensations'
+		},
+	},
+
+	['squares'] = {
+		label = 'squares',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious squares'
+		},
+	},
+
+	['twix'] = {
+		label = 'twix',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious twix'
+		},
+	},
+
+	['twirl'] = {
+		label = 'twirl',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious twirl'
+		},
+	},
+
+	['yorkie'] = {
+		label = 'yorkie',
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			notification = 'You ate a delicious yorkie'
+		},
+	},
